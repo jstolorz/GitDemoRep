9bbc05e5494568fd2575349c92b91c7cdd1c5210
jstolorz <jstolorz@gmail.com>
2017-10-25 14:05:34 +0200

    Add hallo world message

--- src/com/reddragon/Main.java
+++ src/com/reddragon/Main.java
@@ -2,7 +2,17 @@ package com.reddragon;
 
 public class Main {
 
+    private int id;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
